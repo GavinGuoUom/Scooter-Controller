9d481518ed0b1e24dd00db89743820e0920b1997
GavinGuoUoM <jianwei.guo@postgrad.manchester.ac.uk>
2022-05-01 20:21:05 +0100

    add brake to controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "Arduino.h"
+#include "PID_v1.h"
 
 #ifndef DEBUG
 #define DEBUG
@@ -44,7 +45,16 @@ bool drive_flag;
 bool brake_flag;
 uint8_t hall_sen;
 uint8_t switch_patt;
-uint8_t bat_vin;
+uint8_t vol_in;
+uint8_t curr_in;
+double bat_vol;
+double bat_curr;
+double target_curr;
+double target_voltage;
+double PIDinput;
+double PIDoutput;
+
+PID myPID(&PIDinput, &PIDoutput, &target_voltage, 0.02, 0.01, 0.01, DIRECT);
 
 void drive_sw_interrupt();
 void brake_sw_interrupt();
@@ -73,11 +83,6 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(BRAKE_SW_PIN), brake_sw_interrupt, CHANGE);
   pinMode(DRIVE_LED, OUTPUT); //
   // from sensor
-  /*
-  pinMode(HALL_U_PIN, INPUT_PULLUP);
-  pinMode(HALL_V_PIN, INPUT_PULLUP);
-  pinMode(HALL_W_PIN, INPUT_PULLUP);
-  */
   pinMode(BAT_VO_PIN, INPUT);
   pinMode(BAT_CURR_PIN, INPUT);
   // Enable pin change interrupt
@@ -85,44 +90,50 @@ void setup() {
   PCMSK1 |= (1 << PCINT8) | (1 << PCINT9) | (1 << PCINT10);
   PORTC |= (1<<PC0) | (1<<PC1) | (1<<PC2); // pinMode()
   // from voltage
-  //  pinMode(BAT_VO_PIN, INPUT_PULLUP);
   #ifdef DEBUG
   Serial.begin(9600);
   #endif
 
+  // Set up PID
+  PIDinput = 0;
+  PIDoutput = 0;
+  target_voltage = 42;
+  target_curr = -1;
+  myPID.SetMode(AUTOMATIC);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  bat_vin = analogRead(BAT_VO_PIN);
+  vol_in = analogRead(BAT_VO_PIN);
 
-  switch (hall_sen)
+  if(drive_flag && !brake_flag)
   {
-    // HW HV HU
-  case 1: //0b001
-    switch_patt = (1<<UH) | (1<<VL);
-    break;
-  case 3: //0b011
-    switch_patt = (1<<UH) | (1<<WL);
-    break;
-  case 2: //0b010
-    switch_patt = (1<<VH) | (1<<WL);
-    break;
-  case 6: //0b110
-    switch_patt = (1<<VH) | (1<<UL);
-    break;
-  case 4: //0b100
-    switch_patt = (1<<WH) | (1<<UL);
-    break;
-  case 5: //0b101
-    switch_patt = (1<<WH) | (1<<VL);
-    break;
-  default:
-    switch_patt = (1<<UH) | (1<<VL);
-    break;
-  }
-  if(drive_flag)
-  {
+    //drive mode
+    switch (hall_sen)
+    {
+      case 1: //0b001
+        switch_patt = (1<<UH) | (1<<VL);
+        break;
+      case 3: //0b011
+        switch_patt = (1<<UH) | (1<<WL);
+        break;
+      case 2: //0b010
+        switch_patt = (1<<VH) | (1<<WL);
+        break;
+      case 6: //0b110
+        switch_patt = (1<<VH) | (1<<UL);
+        break;
+      case 4: //0b100
+        switch_patt = (1<<WH) | (1<<UL);
+        break;
+      case 5: //0b101
+        switch_patt = (1<<WH) | (1<<VL);
+        break;
+      default:
+        switch_patt = (1<<UH) | (1<<VL);
+        break;
+    }
+
     PORTB = switch_patt;
     //digitalWrite(PWM_H_PIN, HIGH);
     //digitalWrite(PWM_L_PIN, HIGH);
@@ -135,11 +146,46 @@ void loop() {
     #endif
 
   }
-  else{
-    PORTB = 0;
-    // analogWrite(PWM_H_PIN, 0);
-    // analogWrite(PWM_L_PIn, 0);
+  else if(!drive_flag && brake_flag)
+  {
+    //brake mode
+    switch (hall_sen)
+    {
+      case 1: //0b001
+        switch_patt = (1<<UL);
+        break;
+      case 3: //0b011
+        switch_patt = (1<<UL);
+        break;
+      case 2: //0b010
+        switch_patt = (1<<VL);
+        break;
+      case 6: //0b110
+        switch_patt = (1<<VL);
+        break;
+      case 4: //0b100
+        switch_patt = (1<<WL);
+        break;
+      case 5: //0b101
+        switch_patt = (1<<WL);
+        break;
+      default:
+        switch_patt = (1<<UL);
+        break;
+      }
+    
+    #ifdef DEBUG
+    Serial.print("Switch pattern:");
+    Serial.println(switch_patt,BIN);
+    Serial.print("\n");
+    #endif
+
+    PORTB = switch_patt; 
+    analogWrite(PWM_H_PIN, 0);
+    myPID.Compute();
+    analogWrite(PWM_L_PIN, (int)PIDoutput);
   }
+
 }
 
 void drive_sw_interrupt()
@@ -150,12 +196,17 @@ void drive_sw_interrupt()
   Serial.println(drive_flag);
   Serial.print("\n");
   #endif
-  
+  digitalWrite(DRIVE_LED, drive_flag);
 }
 
 void brake_sw_interrupt()
 {
   brake_flag = digitalRead(BRAKE_SW_PIN);
+  #ifdef DEBUG
+  Serial.print("Detected drive switch:");
+  Serial.println(drive_flag);
+  Serial.print("\n");
+  #endif
 }
 
 ISR(PCINT1_vect)
