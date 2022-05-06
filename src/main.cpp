0ccbb52a5e0eb8fce9895dbe9caf3846f9fc7b43
GavinGuoUoM <jianwei.guo@postgrad.manchester.ac.uk>
2022-05-06 11:01:09 +0100

    switch pattern verified

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include "Arduino.h"
 #include "PID_v1.h"
+#include "time.h"
 
 #ifndef DEBUG
 #define DEBUG
@@ -53,11 +54,18 @@ double target_curr;
 double target_voltage;
 double PIDinput;
 double PIDoutput;
+int hall_c=0;
+
+unsigned long time_0=0;
+unsigned long time_1=0;
 
 PID myPID(&PIDinput, &PIDoutput, &target_voltage, 0.02, 0.01, 0.01, DIRECT);
 
 void drive_sw_interrupt();
 void brake_sw_interrupt();
+void switch_pattn();
+void switch_pattn2();
+void switch_pattn3();
 
 
 void setup() {
@@ -79,8 +87,8 @@ void setup() {
   // from control swtiches
   pinMode(DRIVE_SW_PIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(DRIVE_SW_PIN), drive_sw_interrupt, CHANGE);
-  pinMode(BRAKE_SW_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(BRAKE_SW_PIN), brake_sw_interrupt, CHANGE);
+  // pinMode(BRAKE_SW_PIN, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(BRAKE_SW_PIN), brake_sw_interrupt, CHANGE);
   pinMode(DRIVE_LED, OUTPUT); //
   // from sensor
   pinMode(BAT_VO_PIN, INPUT);
@@ -100,121 +108,163 @@ void setup() {
   target_voltage = 42;
   target_curr = -1;
   myPID.SetMode(AUTOMATIC);
+
+  switch_pattn();
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //drive_flag = true;
+  brake_flag = false;
   vol_in = analogRead(BAT_VO_PIN);
-
-  if(drive_flag && !brake_flag)
+  // hall_sen = PINC & 0b000111;
+  switch_pattn();
+  if(drive_flag)
   {
-    //drive mode
-    switch (hall_sen)
-    {
-      case 1: //0b001
-        switch_patt = (1<<UH) | (1<<VL);
-        break;
-      case 3: //0b011
-        switch_patt = (1<<UH) | (1<<WL);
-        break;
-      case 2: //0b010
-        switch_patt = (1<<VH) | (1<<WL);
-        break;
-      case 6: //0b110
-        switch_patt = (1<<VH) | (1<<UL);
-        break;
-      case 4: //0b100
-        switch_patt = (1<<WH) | (1<<UL);
-        break;
-      case 5: //0b101
-        switch_patt = (1<<WH) | (1<<VL);
-        break;
-      default:
-        switch_patt = (1<<UH) | (1<<VL);
-        break;
-    }
-
-    PORTB = switch_patt;
-    //digitalWrite(PWM_H_PIN, HIGH);
-    //digitalWrite(PWM_L_PIN, HIGH);
     analogWrite(PWM_H_PIN, 255);
     analogWrite(PWM_L_PIN, 255);
-    #ifdef DEBUG
-    Serial.print("Switch pattern:");
-    Serial.println(switch_patt,BIN);
-    Serial.print("\n");
-    #endif
-
   }
-  else if(!drive_flag && brake_flag)
-  {
-    //brake mode
-    switch (hall_sen)
-    {
-      case 1: //0b001
-        switch_patt = (1<<UL);
-        break;
-      case 3: //0b011
-        switch_patt = (1<<UL);
-        break;
-      case 2: //0b010
-        switch_patt = (1<<VL);
-        break;
-      case 6: //0b110
-        switch_patt = (1<<VL);
-        break;
-      case 4: //0b100
-        switch_patt = (1<<WL);
-        break;
-      case 5: //0b101
-        switch_patt = (1<<WL);
-        break;
-      default:
-        switch_patt = (1<<UL);
-        break;
-      }
-    
-    #ifdef DEBUG
-    Serial.print("Switch pattern:");
-    Serial.println(switch_patt,BIN);
-    Serial.print("\n");
-    #endif
-
-    PORTB = switch_patt; 
-    analogWrite(PWM_H_PIN, 0);
-    myPID.Compute();
-    analogWrite(PWM_L_PIN, (int)PIDoutput);
-  }
-
 }
 
 void drive_sw_interrupt()
 {
-  drive_flag = digitalRead(DRIVE_SW_PIN);
-  #ifdef DEBUG
-  Serial.print("Detected drive switch:");
-  Serial.println(drive_flag);
-  Serial.print("\n");
-  #endif
-  digitalWrite(DRIVE_LED, drive_flag);
+  time_1 = millis();
+  if (time_1 - time_0>=10)
+  {
+    drive_flag = digitalRead(DRIVE_SW_PIN);
+    #ifdef DEBUG
+    Serial.print("Detected drive switch:");
+    Serial.println(drive_flag);
+    Serial.print("\t Time:");
+    Serial.println(time_0);
+    Serial.print("\n");
+    #endif
+    digitalWrite(DRIVE_LED, drive_flag);
+  }
+  
 }
 
 void brake_sw_interrupt()
 {
-  brake_flag = digitalRead(BRAKE_SW_PIN);
-  #ifdef DEBUG
-  Serial.print("Detected drive switch:");
-  Serial.println(drive_flag);
-  Serial.print("\n");
-  #endif
+  time_1 = millis();
+  if (time_1-time_0>=10)
+  {
+    time_0=time_1;
+    brake_flag = digitalRead(BRAKE_SW_PIN);
+    #ifdef DEBUG
+    Serial.print("Detected drive switch:");
+    // Serial.println(drive_flag);
+    Serial.print("\t Time:");
+    Serial.println(time_0);
+    Serial.print("\n");
+    #endif
+  }
 }
 
 ISR(PCINT1_vect)
 {
-  hall_sen = PINC & 0b000111; // W||V||U
+  hall_c ++;
+  hall_sen = PINC & 0b00000111;
+  switch_pattn();
   #ifdef DEBUG
   Serial.write("Detect hall sensor");
+  Serial.println(hall_c);
+  Serial.print("\t");
   Serial.println(hall_sen,BIN);
   Serial.print("\n");
-  #endif
+  Serial.print("Switch pattern:");
+  Serial.println(switch_patt, BIN);
+  Serial.print("\n");
+  #endif   
+}
+
+void switch_pattn()
+{
+    //hall_sen = ~(PINC & 0b00000111) & 0b00000111; // W||V||U
+    switch (hall_sen)
+    {
+      case 0b001: //0b001
+        switch_patt = (1<<WH) | (1<<VL);
+        break;
+      case 0b011: //0b011
+        switch_patt = (1<<UH) | (1<<VL);
+        break;
+      case 0b010: //0b010
+        switch_patt = (1<<UH) | (1<<WL);
+        break;
+      case 0b110: //0b110
+        switch_patt = (1<<VH) | (1<<WL);
+        break;
+      case 0b100: //0b100
+        switch_patt = (1<<VH) | (1<<UL);
+        break;
+      case 0b101: //0b101
+        switch_patt = (1<<WH) | (1<<UL);
+        break;
+      default:
+        switch_patt = 0;
+        break;
+    }
+    PORTB = switch_patt;
+}
+
+void switch_pattn2()
+{
+    hall_sen = ~(PINC & 0b00000111) & 0b00000111; // W||V||U
+    switch (hall_sen)
+    {
+      case 0b001: //0b001
+        switch_patt = (1<<WH) | (1<<UL);
+        break;
+      case 0b011: //0b011
+        switch_patt = (1<<WH) | (1<<VL);
+        break;
+      case 0b010: //0b010
+        switch_patt = (1<<UH) | (1<<VL);
+        break;
+      case 0b110: //0b110
+        switch_patt = (1<<UH) | (1<<WL);
+        break;
+      case 0b100: //0b100
+        switch_patt = (1<<VH) | (1<<WL);
+        break;
+      case 0b101: //0b101
+        switch_patt = (1<<VH) | (1<<UL);
+        break;
+      default:
+        switch_patt = 0;
+        break;
+    }
+    PORTB = switch_patt;
+}
+
+void switch_pattn3()
+{
+    hall_sen = ~(PINC & 0b00000111) & 0b00000111; // W||V||U
+    switch (hall_sen)
+    {
+      case 0b001: //0b001
+        switch_patt = (1<<UH) | (1<<WL);
+        break;
+      case 0b011: //0b011
+        switch_patt = (1<<VH) | (1<<WL);
+        break;
+      case 0b010: //0b010
+        switch_patt = (1<<VH) | (1<<UL);
+        break;
+      case 0b110: //0b110
+        switch_patt = (1<<WH) | (1<<UL);
+        break;
+      case 0b100: //0b100
+        switch_patt = (1<<WH) | (1<<VL);
+        break;
+      case 0b101: //0b101
+        switch_patt = (1<<UH) | (1<<VL);
+        break;
+      default:
+        switch_patt = 0;
+        break;
+    }
+    
+    PORTB = switch_patt;
 }
